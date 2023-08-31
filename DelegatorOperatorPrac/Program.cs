1ec88e615e4c1ef9094b3d979ad0b31dff1a003f
yes25 <programmer.yes25@gmail.com>
2023-08-31 14:42:08 +0900

    초기 커밋.

---
+++ DelegatorOperatorPrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorOperatorPrac
+{
+    internal class Program
+    {
+        public delegate void SendString(string messge);
+
+        static void Main(string[] args)
+        {
+            SendString seyHello;
+        }
+    }
+}
